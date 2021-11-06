743d529250dffe386a155278f6008ed579afe6ef
Мария <lpvamaria@gmail.com>
2021-11-06 18:45:42 +0200

    Получение данных

---
+++ js/menu.js
@@ -0,0 +1,14 @@
+const restourant = 'food-band';
+
+const renderItems = (data) => {
+    console.log(data);
+}
+
+fetch(`./db/${restourant}.json`)
+    .then((response) => response.json())
+    .then((data) => {
+        renderItems(data)
+    })
+    .catch((error) => {
+        console.log(error)
+    })
